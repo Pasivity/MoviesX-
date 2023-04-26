e6759e6d1ea1caf50ef72b5cb0252a4f2a232173
Pasivity <90650674+Pasivity@users.noreply.github.com>
2023-04-26 13:12:27 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-async function main() {
-  const movies = await fetch(
-    "https://www.omdbapi.com/?apikey=27cbe55&s=Batman"
-  );
+const apiKey = "27cbe55";
+const apiUrl = "https://www.omdbapi.com/";
 
-  document.body.classList += " movies__loading";
+async function fetchMovies(searchQuery) {
+  const response = await fetch(`${apiUrl}?apikey=${apiKey}&s=${searchQuery}`);
+  const data = await response.json();
+  return data;
+}
 
-  const moviesData = await movies.json();
+async function loadInitialMovies() {
+  const moviesData = await fetchMovies("Batman");
+  displayMovies(moviesData.Search);
+}
 
-  document.body.classList.remove("movies__loading");
-
+function displayMovies(movies) {
   const movieListEl = document.querySelector(".movies");
-  movieListEl.innerHTML = moviesData.Search.map(
+  movieListEl.innerHTML = movies.map(
     (movie) =>
       `<div class="movie">
             <figure class="movie__img--wrapper">
@@ -19,7 +23,7 @@ async function main() {
                 <div class="movie__title">${movie.Title}</div>
                 <div class="movie__year">(${movie.Year})</div>
                 <div class="movie__genres">
-                <div class="movie__genre">${movie.Type}</div>
+                  <div class="movie__genre">${movie.Type}</div>
                 </div>
               </div>
             </figure>
@@ -27,49 +31,29 @@ async function main() {
   ).join("");
 }
 
-// Add a 2-second delay for to let DOM elements load with JS
-setTimeout(() => {
-  main();
-}, 2000);
-
 function movieForm(event) {
   event.preventDefault(); // prevent page refresh
-
-  document.querySelector('.form__wrapper').removeEventListener('submit', movieForm);
 }
 
 async function onSearchChange(event) {
   const search = event.target.value;
   try {
-    let loader = document.querySelector('.movies__loading');
-    
-    document.getElementsByClassName('movies').innerHTML = loader
-    const movies = await fetch(
-      `https://www.omdbapi.com/?apikey=27cbe55&s=${search}`
-    );
-    const moviesData = await movies.json();
-    const movieListEl = document.querySelector(".movies");
-    // Returns the first six movies
-    movieListEl.innerHTML = moviesData.Search.splice(0,6).map(
-      (movie) =>
-        ` <p class="movie__result">Search results for: "${search}"</p>
-          <div class="movie">
-            <figure class="movie__img--wrapper">
-              <img class="movie__img" src="${movie.Poster}" alt="">
-              <div class="movie__description">
-                <div class="movie__title">${movie.Title}</div>
-                <div class="movie__year">(${movie.Year})</div>
-                <div class="movie__genres">
-                <div class="movie__genre">${movie.Type}</div>
-                </div>
-              </div>
-            </figure>
-          </div>`
-    ).join("");
+    const moviesData = await fetchMovies(search);
+    displayMovies(moviesData.Search.splice(0, 6));
   } catch (error) {
-    // display an error message to the user
+    // display an error message to the user if movie not found when searched
     const movieListEl = document.querySelector(".movies");
     movieListEl.innerHTML = `<div class="error-message">Sorry, we couldn't find any matching results for your search.</div>`;
   }
 }
 
+function main() {
+  // Add a 2-second delay to let DOM elements load with JS
+  setTimeout(() => {
+    loadInitialMovies();
+  }, 2000);
+
+  onSearchChange();
+}
+
+main();
